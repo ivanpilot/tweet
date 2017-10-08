b11fe4e891f1a9e96879b491896cb7e8808c210b
Ivan Pilot <ivanpilot@gmail.com>
2017-10-09 00:47:07 +0200

    reset db and adjust FactoryGirl to seed the db

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,13 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# FactoryGirl.create_list(:user, 10)
-# FactoryGirl.create_list(:post, 20)
-
 10.times do
-  Post.create(
-    itle { Faker::Lorem.word}
-    body { Faker::Lorem.sentence}
-    association :user
-  )
+  FactoryGirl.create(:post)
 end
--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -2,6 +2,6 @@ FactoryGirl.define do
   factory :user do
     username { Faker::Internet.unique.user_name }
     email { Faker::Internet.unique.email }
-    password { Faker::Internet.password }
+    password "password"
   end
 end
